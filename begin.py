eadaad4c742e86d1f9b361025f0b6e0a89e8e8f9
Ragav Venkatesan <ragav.venkatesan@asu.edu>
2017-03-12 17:17:44 -0700

    Setup.

--- begin.py
+++ begin.py
@@ -1,7 +1,7 @@
 if __name__ == '__main__':
     
     import sys
-    from incremental_gan import igan
+    from mnist import igan
     from dataset import cook_split_base as base_dataset
     from dataset import cook_split_inc as inc_dataset    
     # from dataset import cook_mnist_complete as base_dataset 
@@ -18,17 +18,18 @@ if __name__ == '__main__':
     
     lr = (0.04, 0.005, 0.001)
     epochs =(15, 15)
-    igan_obj.train_init_gan ( lr = lr, epochs = epochs, verbose = 2 )
+    # igan_obj.train_init_gan ( lr = lr, epochs = epochs, verbose = 2 )
 
     # For all the MLP, we use the same learning rates and epochs.
     # For optimizers, go inside the setup function and change them.
-    lr = (0.05, 0.01)
+    lr = (0.05, 0.01)    
     epochs =(10)
     igan_obj.train_base_mlp ( lr =lr, epochs = epochs, verbose = 2 )    
 
     # This will initialize the baseline incremental network.
     # This network is intended to demonstrate catastrophic forgetting
     # will be training with the increment dataset.
+    lr = (0.05, 0.0001)
     igan_obj.setup_baseline_inc ( inc, verbose = 2 )
     igan_obj.train_baseline_inc ( lr =lr, epochs = epochs, verbose = 2 )
 
@@ -36,6 +37,7 @@ if __name__ == '__main__':
     # This network is intended to deomonstrate that hallucinating from GAN 
     # could allow us to learn incremental learning. This is counter to 
     # what the baseline demonstrates.
+    lr = (0.05, 0.01)
     igan_obj.setup_mentor (verbose = 2)
     igan_obj.setup_hallucinated_inc ( inc, verbose = 2 )
-    igan_obj.train_hallucinated_inc ( lr =lr, epochs = epochs, verbose = 2 )    
+    # igan_obj.train_hallucinated_inc ( lr =lr, epochs = epochs, verbose = 2 )    
