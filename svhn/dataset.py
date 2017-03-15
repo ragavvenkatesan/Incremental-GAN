a908e229e16b5cfd4fe52060635da0d99fb35dbd
Ragav Venkatesan (Student) <ASUAD\rvenka10@en4113745l.fulton.ad.asu.edu>
2017-03-14 22:11:32 -0700

    working on svhn

---
+++ svhn/dataset.py
@@ -0,0 +1,196 @@
+from yann.special.datasets import split_all, split_only_train
+
+def cook_svhn_complete(location, verbose = 1, **kwargs):
+    """
+    Wrapper to cook svhn dataset that creates the whole thing. Will take as input,
+
+    Args:
+        location: Where are the matlab files.
+        save_directory: which directory to save the cooked dataset onto.
+        dataset_parms: default is the dictionary. Refer to :mod:`setup_dataset`
+        preprocess_params: default is the dictionary. Refer to :mod:`setup_dataset`
+
+    Notes:
+
+        This will also have the split parameter.
+    """
+    if not 'data_params' in kwargs.keys():
+        data_params = {
+                   "source"             : 'matlab',
+                   # "name"               : 'yann_svhn', # some name.
+                   "location"     : location,    # some location to load from.  
+                   "height"             : 32,
+                   "width"              : 32,
+                   "channels"           : 3,
+                   "batches2test"       : 13,
+                   "batches2train"      : 100,
+                   "batches2validate"   : 13,
+                   "mini_batch_size"    : 500}
+    else:
+        data_params = kwargs['data_params']
+
+    if not 'preprocess_params' in kwargs.keys():
+
+    # parameters relating to preprocessing.
+        preprocess_params = {
+                            "normalize"     : True,
+                            "ZCA"           : False,
+                            "grayscale"     : False,
+                            "zero_mean"     : True,
+                        }
+    else:
+        preprocess_params = kwargs['preprocess_params']
+
+    if not 'save_directory' in kwargs.keys():
+        save_directory = '_datasets'
+    else:
+        save_directory = kwargs ['save_directory']
+
+    if not 'splits' in  kwargs.keys():
+        splits = { 
+                        "base"              : [0,1,2,3,4,5,6,7,8,9],
+                        "shot"              : [],
+                        "p"                 : 0
+                    }     
+    else:
+        splits = kwargs ['splits']
+
+    dataset = split_only_train(dataset_init_args = data_params,
+                            save_directory = save_directory,
+                            preprocess_init_args = preprocess_params,
+                            split_args = splits,
+                            verbose = 3)
+    return dataset
+
+def cook_split_base(location, verbose = 1, **kwargs):
+    """
+    Wrapper to cook svhn dataset that also creates a split dataset. Will take as input,
+
+    Args:
+        location: Where are the matlab files.
+        save_directory: which directory to save the cooked dataset onto.
+        dataset_parms: default is the dictionary. Refer to :mod:`setup_dataset`
+        preprocess_params: default is the dictionary. Refer to :mod:`setup_dataset`
+
+    Notes:
+        
+        The base of this dataset will be classes 0,1,2,4,5 and the split will be classes
+        6,7,8,9.
+    """
+
+    if not 'data_params' in kwargs.keys():
+        data_params = {
+                   "source"             : 'matlab',
+                   # "name"               : 'yann_svhn', # some name.
+                   "location"     : location,    # some location to load from.  
+                   "height"             : 32,
+                   "width"              : 32,
+                   "channels"           : 3,
+                   "batches2test"       : 13,
+                   "batches2train"      : 100,
+                   "batches2validate"   : 13,
+                   "mini_batch_size"    : 500}
+
+    else:
+        data_params = kwargs['data_params']
+
+    if not 'preprocess_params' in kwargs.keys():
+
+    # parameters relating to preprocessing.
+        preprocess_params = {
+                            "normalize"     : True,
+                            "ZCA"           : False,
+                            "grayscale"     : False,
+                            "zero_mean"     : True,
+                        }
+    else:
+        preprocess_params = kwargs['preprocess_params']
+
+    if not 'save_directory' in kwargs.keys():
+        save_directory = '_datasets'
+    else:
+        save_directory = kwargs ['save_directory']
+
+    if not 'splits' in  kwargs.keys():
+        splits = { 
+                        "base"              : [0,1,2,3,4,5],
+                        "shot"              : [6,7,8,9],
+                        "p"                 : 0
+                    }     
+    else:
+        splits = kwargs ['splits']
+
+    dataset = split_all(dataset_init_args = data_params,
+                            save_directory = save_directory,
+                            preprocess_init_args = preprocess_params,
+                            split_args = splits,
+                            verbose = 3)
+    return dataset
+
+def cook_split_inc(verbose = 1, **kwargs):
+    """
+    Wrapper to cook svhn dataset that also creates the rest of the dataset. Will take as input,
+
+    Args:
+        location: Where are the matlab files.
+        save_directory: which directory to save the cooked dataset onto.
+        dataset_parms: default is the dictionary. Refer to :mod:`setup_dataset`
+        preprocess_params: default is the dictionary. Refer to :mod:`setup_dataset`
+
+    Notes:
+        
+        The base of this dataset will be classes 0,1,2,4,5,7,9 and the split will be classes
+        3,6,8.
+    """
+
+    if not 'data_params' in kwargs.keys():
+        data_params = {
+                   "source"             : 'matlab',
+                   # "name"               : 'yann_svhn', # some name.
+                   "location"     : location,    # some location to load from.  
+                   "height"             : 32,
+                   "width"              : 32,
+                   "channels"           : 3,
+                   "batches2test"       : 13,
+                   "batches2train"      : 100,
+                   "batches2validate"   : 13,
+                   "mini_batch_size"    : 500}
+
+    else:
+        data_params = kwargs['data_params']
+
+    if not 'preprocess_params' in kwargs.keys():
+
+    # parameters relating to preprocessing.
+        preprocess_params = {
+                            "normalize"     : True,
+                            "ZCA"           : False,
+                            "grayscale"     : False,
+                            "zero_mean"     : True,
+                        }
+    else:
+        preprocess_params = kwargs['preprocess_params']
+
+    if not 'save_directory' in kwargs.keys():
+        save_directory = '_datasets'
+    else:
+        save_directory = kwargs ['save_directory']
+
+    if not 'splits' in  kwargs.keys():
+        splits = { 
+                        "base"              : [6,7,8,9],
+                        "shot"              : [0,1,2,3,4,5],
+                        "p"                 : 0
+                    }     
+    else:
+        splits = kwargs ['splits']
+
+    dataset = split_only_train(dataset_init_args = data_params,
+                            save_directory = save_directory,
+                            preprocess_init_args = preprocess_params,
+                            split_args = splits,
+                            verbose = 3)
+    return dataset
+
+if __name__ == '__main__':
+    pass
---
+++ svhn/site_2.py
@@ -0,0 +1,73 @@
+import sys, os
+from cifar10 import igan
+from dataset import cook_split_inc as inc_dataset  
+from yann.utils.pickle import load
+
+if __name__ == '__main__':
+    
+    # simply locations
+    gan = 39   # which epoch of gan do you want transfer to site 2 ?    
+    site_1_root = 'records/site_1'
+    root = 'records/site_2/gan_' + str(gan)
+    if not os.path.exists(root):
+        os.makedirs(root)           
+
+    lr = (0.00005, 0.01, 0.001, 1e-4, 1e-5)    
+    epochs =(15, 15, 10, 10)
+    p_vals = [0, 10, 50, 100, 500, 2000]
+    temperature = 8
+    # setup incremental dataset parameters dataset.
+    temp_splits = { "shot"               : [6,7,8,9],
+                    "base"               : [0,1,2,3,4,5],    
+                    "p"                  : 0   }  
+
+    temp_base_data = inc_dataset (splits = temp_splits, verbose = 1)
+    temp_base_data = temp_base_data.dataset_location()  
+    site2 = igan ( init_dataset = temp_base_data, root = root, verbose = 1 )
+
+    print (". Setup transfered gan network from site 1.")
+    gan_params = load(site_1_root + '/resultor/gan/params/epoch_' + str(gan) +'.pkl')
+    site2.setup_gan(dataset = temp_base_data,
+                    root = root, 
+                    params = gan_params, 
+                    cook = False,
+                    verbose = 1)
+
+    print (". Setup transfered base network from site 1.")    
+    base_params = load(site_1_root + '/resultor/base-network/params/epoch_29.pkl') 
+    site2.setup_base_mlp(   dataset = temp_base_data, 
+                            root = root, 
+                            params = base_params, 
+                            cook = False,
+                            verbose = 1)
+
+    # setup incremental dataset parameters dataset.
+    inc_splits = {  "base"               : [6,7,8,9],
+                    "shot"               : [0,1,2,3,4,5],    
+                    "p"                  : 0   }  
+
+    for p in p_vals:
+        
+        inc_splits ['p'] = p 
+
+        print (". Running p = " + str(p) )
+        inc = inc_dataset (splits = inc_splits, verbose = 1)
+        inc = inc.dataset_location()              
+
+        # This will initialize the baseline incremental network.
+        # This network is intended to demonstrate catastrophic forgetting
+        # will be training with the increment dataset.
+        if not os.path.exists (root + '/p_' + str(p)):
+            os.makedirs(root + '/p_' + str(p))
+        baseline_root = root + '/p_' + str(p) + '/baseline'
+        site2.setup_baseline_inc ( dataset = inc, root = baseline_root, verbose = 1 )
+        site2.train_baseline_inc ( lr =lr, epochs = epochs, verbose = 2 )
+
+        # This will initialize and train the hallucinated incremental network.
+        # This network is intended to deomonstrate that hallucinating from GAN 
+        # could allow us to learn incremental learning. This is counter to 
+        # what the baseline demonstrates.
+        igan_root = root + '/p_' + str(p) + '/igan'        
+        site2.setup_mentor (temperature = temperature, verbose = 1)
+        site2.setup_hallucinated_inc ( dataset = inc, root = igan_root, verbose = 1 )
+        site2.train_hallucinated_inc ( lr =lr, epochs = epochs, verbose = 2 )            
